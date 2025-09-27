d8ede3c49bc9678a7a4f12f5b33507b095c87a5a
asritvardhan <154015063+asritvardhan@users.noreply.github.com>
2025-09-27 15:54:19 +0530

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -87,7 +87,7 @@ const registerUser = async (req, res) => {
 const loginUser = async (req, res) => {
   try {
     const { email, password } = req.body;
-
+    console.log(email,password);
     if (!email || !password) {
       return res.status(400).json({ message: "Please provide email and password" });
     }
